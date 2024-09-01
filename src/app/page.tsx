5442f0785cd9efaa4f722b12d0e8c06df20d4039
Will Kessler <willkessler@gmail.com>
2024-09-01 10:27:40 -0700

    added visual separator

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <main className="container mx-auto p-4">
       <h1 className="text-2xl font-bold mb-4">Bill of Lading Generator</h1>
       <BillOfLadingGenerator />
-      <div style={{paddingTop:"20px"}}>
+      <div style={{paddingTop:"40px", marginTop:'40px', borderTop:"2px dotted #eee"}}>
         <BillOfLadingForm />
       </div>
     </main>
